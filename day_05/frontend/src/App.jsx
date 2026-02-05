562876d2c54ed5064ff155d907d4ea46b95e24f3
vaibhav-deval <vaibhavdeval004@gmail.com>
2026-02-05 23:56:27 +0530

    api configured for adding and deleting notes

--- day_05/frontend/src/App.jsx
+++ day_05/frontend/src/App.jsx
@@ -3,22 +3,52 @@ import axios from "axios";
 import "./App.css";
 const App = () => {
   const [notes, setNotes] = useState([]);
+
   useEffect(() => {
     fetchNotes();
   }, []);
 
-  const fetchNotes = async () =>    
+  const fetchNotes = () =>
     axios.get("http://localhost:3000/api/notes").then((res) => {
       setNotes(res.data.notes ? res.data.notes : null);
     });
 
+  const addNote = (e) => {
+    e.preventDefault();
+    const { title, description } = e.target.elements;
+    axios
+      .post("http://localhost:3000/api/notes", {
+        title: title.value,
+        description: description.value,
+      })
+      .then(() => fetchNotes());
+  };
+
+  const deleteNote = (id) => {
+    axios
+      .delete(`http://localhost:3000/api/note/${id}`)
+      .then(() => fetchNotes());
+  };
   return (
-    <div className="app">
+    <div className="app" onSubmit={addNote}>
+      <form className="note-form">
+        <input name="title" type="text" placeholder="Enter Title" />
+        <input name="description" type="text" placeholder="Enter Description" />
+        <button type="submit">Add Note</button>
+      </form>
       <div className="notes">
-        {notes.map((note,idx) => (
-          <div className="note" key={idx }>
+        {notes.map((note, idx) => (
+          <div className="note" key={idx}>
+            <p>S. No. {idx + 1}</p>
             <h1 className="title">{note.title}</h1>
             <p>{note.description}</p>
+            <button
+              onClick={() => {
+                deleteNote(note._id);
+              }}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
