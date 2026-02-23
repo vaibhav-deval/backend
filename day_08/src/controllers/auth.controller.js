94b81345efee3c6a7ce3f7dfde0ab19288f75e1c
vaibhav-deval <vaibhavdeval004@gmail.com>
2026-02-24 01:22:30 +0530

    feat: initialize day_08 project with user authentication
    - add package.json with dependencies for express, mongoose, and dotenv
    - create server.js to set up the Express server and connect to MongoDB
    - implement app.js to configure middleware and routes for authentication
    - add database.js for MongoDB connection logic
    - create auth.controller.js for user registration and login logic
    - define user.model.js for user schema with validation
    - set up auth.routes.js for handling authentication routes

---
+++ day_08/src/controllers/auth.controller.js
@@ -0,0 +1,80 @@
+const crypto = require("crypto");
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+async function registerController(req, res) {
+  console.log(req.body);
+
+  const { name, username, email, password, bio, profilePicture } = req.body;
+
+  const isUserExists = await userModel.findOne({
+    $or: [{ email }, { username }],
+  });
+  if (isUserExists) {
+    return res.status(400).json({
+      message:
+        "User already exists" +
+        (isUserExists.email == email
+          ? "Email already exists"
+          : "Username already exists"),
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const user = await userModel.create({
+    name,
+    username,
+    email,
+    password: hash,
+    bio,
+  });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token);
+  res.status(201).json({
+    message: "User registered successfully",
+    user: {
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profilePicture: user.profilePicture,
+    },
+  });
+}
+
+async function loginController(req, res) {
+  const { email, username, password } = req.body;
+  const user = await userModel.findOne({
+    $or: [{ email: email }, { username: username }],
+  });
+  if (!user) {
+    return res.status(409).json({ message: "user not found" });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+  if (hash !== user.password) {
+    return res.status(409).json({ message: "Invalid credentials" });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  res.cookie("token", token);
+  res.status(200).json({
+    message: "User logged in successfully",
+    user: {
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profilePicture: user.profilePicture,
+    },
+  });
+}
+
+module.exports = { registerController, loginController };
